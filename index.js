41064f20a6892031bc22a136302680b29578d5c6
Esmeralda Quintana <esmeralda@firedrivendevelopment.net>
2017-08-03 14:18:47 -0400

    convert markdown files into html files into dest folder

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-console.log('Hello world~');
+const showdown = require('showdown');
+const fs = require('fs');
+const converter = new showdown.Converter();
+const readlineSync = require('readline-sync');
+
+const fileSource = './markdownExamples/';
+const destination = './dest/';
+
+// if destination folder doesn't exist, create it.
+if (!fs.existsSync(destination)){
+    fs.mkdirSync(destination);
+}
 
 
+const files = fs.readdirSync(fileSource);
+
+files.forEach( function(value) {
+    const text = converter.makeHtml(fs.readFileSync(fileSource + value, "utf8"));
+    const title = readlineSync.question('Name file? ' + value + ' ');
+    console.log('named: ' + title);
+    const fullPathDest = destination + title + '.html';
+    fs.writeFileSync(fullPathDest, text);
+    console.log(fs.readFileSync(fullPathDest, "utf8"));
+    console.log('file read...====================');
+    console.log();
+});
+console.log();
+console.log('++++++++++++++++++++++++++++++++++++++++++++');
+console.log('Program exited!');
