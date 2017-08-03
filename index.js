e8aa3baabaf2df0a7dc34019cddee88ed0369f7f
Esmeralda Quintana <esmeralda@firedrivendevelopment.net>
2017-08-03 14:31:27 -0400

    add interactive folder name handling.

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const fs = require('fs');
 const converter = new showdown.Converter();
 const readlineSync = require('readline-sync');
 
-const fileSource = './markdownExamples/';
-const destination = './dest/';
+let folderName = readlineSync.question('Name of folder with markdown files?: ');
+const filesSrc = './' + folderName + '/';
+
+let folderDestName = readlineSync.question('Desired name for destiation folder?: ');
+const destination = './' + folderDestName + '/';
 
 // if destination folder doesn't exist, create it.
 if (!fs.existsSync(destination)){
@@ -12,17 +15,15 @@ if (!fs.existsSync(destination)){
 }
 
 
-const files = fs.readdirSync(fileSource);
+const files = fs.readdirSync(filesSrc);
 
 files.forEach( function(value) {
-    const text = converter.makeHtml(fs.readFileSync(fileSource + value, "utf8"));
+    const text = converter.makeHtml(fs.readFileSync(filesSrc + value, "utf8"));
     const title = readlineSync.question('Name file? ' + value + ' ');
-    console.log('named: ' + title);
     const fullPathDest = destination + title + '.html';
     fs.writeFileSync(fullPathDest, text);
-    console.log(fs.readFileSync(fullPathDest, "utf8"));
-    console.log('file read...====================');
-    console.log();
+    // console.log(fs.readFileSync(fullPathDest, "utf8"));
+    console.log(fullPathDest + ' created!');
 });
 console.log();
 console.log('++++++++++++++++++++++++++++++++++++++++++++');
