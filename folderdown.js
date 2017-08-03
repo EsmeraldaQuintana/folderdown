ce0e8af088e87177921d87454e6e352801f35816
Esmeralda Quintana <esmeralda@firedrivendevelopment.net>
2017-08-03 18:53:35 -0400

    finish 1.0.0 of folderdown

--- folderdown.js
+++ folderdown.js
@@ -1,47 +1,51 @@
-const showdown = require('showdown');
 const fs = require('fs');
-const converter = new showdown.Converter();
 const readlineSync = require('readline-sync');
+const S = require('string');
+const showdown = require('showdown');
+const converter = new showdown.Converter();
 
-// THIS CODE DOESN'T COMPLE
-// I PUT IN PARAMS
-// AND DIDN'T CARRY CHANGE THROUGHOUT
-// ALSO
-// WILL BE REUSING THIS CODE FOR THE CLI VERSION
+console.log('folderdown: starting process.');
+
+/**
+ * Begins the folderdown process.
+ * @param  {string} srcFolder  The folder holding the markdown files.
+ * @param  {string} destFolder The destination folder for converted files.
+ */
 function run(srcFolder, destFolder) {
-    let folderName = readlineSync.question('Name of folder with markdown files?: ');
-    let filesSrc = './' + folderName + '/';
     let tries = 0;
-    while (!fs.existsSync(filesSrc) && tries<3){
-    tries++;
-    if (tries === 3) {
-        console.log('Fuck off.');
+    while (!fs.existsSync(srcFolder) && tries<3){
+        tries++;
+        if (tries === 3) {
+            console.log(`folderdown: Error! Source folder not found after
+                three tries.`);
+            process.exit(0);
+        }
+        console.log(`Folder not found! Try again. `);
+        srcFolder = readlineSync.question(`Path of folder with markdown files?: `);
+    }
+    console.log(`folderdown: ${srcFolder} found.`);
+
+    if (!fs.existsSync(destFolder)){
+        fs.mkdirSync(destFolder);
+        console.log(`folderdown: ${destFolder} created.`);
+    }
+    console.log(`folderdown: ${destFolder} found.`);
+
+    const files = fs.readdirSync(srcFolder);
+    if (files.length === 0) {
+        console.log('Error: No markdown files seen.');
         process.exit(0);
     }
-    console.log('Folder not found! Try again~ ');
-    folderName = readlineSync.question('Name of folder with markdown files?: ');
-    filesSrc = './' + folderName + '/';
-    }
-
-    let folderDestName = readlineSync.question('Desired name for destination folder?: ');
-    const destination = './' + folderDestName + '/';
-    if (!fs.existsSync(destination)){
-    fs.mkdirSync(destination);
-    }
-
-    const files = fs.readdirSync(filesSrc);
 
     files.forEach( function(value) {
-    const text = converter.makeHtml(fs.readFileSync(filesSrc + value, "utf8"));
-    const title = readlineSync.question('Name for ' + value + '? ');
-    const fullPathDest = destination + title + '.html';
-    fs.writeFileSync(fullPathDest, text);
-    // console.log(fs.readFileSync(fullPathDest, "utf8"));
-    console.log(fullPathDest + ' created.');
+        const text = converter.makeHtml(
+            fs.readFileSync(srcFolder + value, "utf8"));
+        const fullPathDest = `${destFolder}${S(value).left(value.length-3).s}.html`;
+        fs.writeFileSync(fullPathDest, text);
+        // console.log(fs.readFileSync(fullPathDest, "utf8"));
+        console.log('folderdown: ' + fullPathDest + ' created.');
     });
-
-    console.log();
-    console.log('folderdown exited, no errors.');
+    console.log('folderdown: exited, no errors.');
 }
 
-exports.run;
+exports.run = run;
