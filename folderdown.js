16a2d6acd375aeb2fde4d423b7b4033247cd5af2
Esmeralda Quintana <esmeralda@firedrivendevelopment.net>
2017-08-03 17:23:51 -0400

    begin restructuring for publishing

--- folderdown.js
+++ folderdown.js
@@ -3,10 +3,16 @@ const fs = require('fs');
 const converter = new showdown.Converter();
 const readlineSync = require('readline-sync');
 
-let folderName = readlineSync.question('Name of folder with markdown files?: ');
-let filesSrc = './' + folderName + '/';
-let tries = 0;
-while (!fs.existsSync(filesSrc) && tries<3){
+// THIS CODE DOESN'T COMPLE
+// I PUT IN PARAMS
+// AND DIDN'T CARRY CHANGE THROUGHOUT
+// ALSO
+// WILL BE REUSING THIS CODE FOR THE CLI VERSION
+function run(srcFolder, destFolder) {
+    let folderName = readlineSync.question('Name of folder with markdown files?: ');
+    let filesSrc = './' + folderName + '/';
+    let tries = 0;
+    while (!fs.existsSync(filesSrc) && tries<3){
     tries++;
     if (tries === 3) {
         console.log('Fuck off.');
@@ -15,24 +21,27 @@ while (!fs.existsSync(filesSrc) && tries<3){
     console.log('Folder not found! Try again~ ');
     folderName = readlineSync.question('Name of folder with markdown files?: ');
     filesSrc = './' + folderName + '/';
-}
+    }
 
-let folderDestName = readlineSync.question('Desired name for destination folder?: ');
-const destination = './' + folderDestName + '/';
-if (!fs.existsSync(destination)){
+    let folderDestName = readlineSync.question('Desired name for destination folder?: ');
+    const destination = './' + folderDestName + '/';
+    if (!fs.existsSync(destination)){
     fs.mkdirSync(destination);
-}
+    }
 
-const files = fs.readdirSync(filesSrc);
+    const files = fs.readdirSync(filesSrc);
 
-files.forEach( function(value) {
+    files.forEach( function(value) {
     const text = converter.makeHtml(fs.readFileSync(filesSrc + value, "utf8"));
     const title = readlineSync.question('Name for ' + value + '? ');
     const fullPathDest = destination + title + '.html';
     fs.writeFileSync(fullPathDest, text);
     // console.log(fs.readFileSync(fullPathDest, "utf8"));
     console.log(fullPathDest + ' created.');
-});
+    });
 
-console.log();
-console.log('Program exited, no errors.');
+    console.log();
+    console.log('folderdown exited, no errors.');
+}
+
+exports.run;
